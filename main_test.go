97042851ce9c04976f7023662b33823853879180
ev-i <47784335+ev-i@users.noreply.github.com>
2026-07-18 14:27:32 +0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -10,7 +10,7 @@ func TestMaxInt(t *testing.T) {
     res := MaxInt(a, b)
 
     if res != b {
-        t.Errorf("expected %d, got %d", b, res)
+        t.Errorf("Expected %d, got %d", b, res)
     }
 }
 
